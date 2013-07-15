0b1291191bac3c1bb72bc5a878e8945dd77bc78b
Gabriel Zea <zea@randomlab.net>
2013-07-15 01:02:42 -0500

    initial dev commit

---
+++ content/pages/about.md
@@ -0,0 +1,5 @@
+Title: About
+Slug: about_comdasuar
+Lang: en
+
+Soon info!
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,15 +2,13 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Andres Cabrera'
-SITENAME = u'Computador Musical Digital Asuar'
+AUTHOR = u'Andres Burbano, Andres Cabrera'
+SITENAME = u'Comdasuar MK II'
 SITEURL = ''
 
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
-
-THEME = 'cebong'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -18,15 +16,11 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-#LINKS =  (('Comdasuar en Github', 'http://github.com/mantaraya36/asuar'),
-#          ('Python.org', 'http://python.org/'),
-#          ('Jinja2', 'http://jinja.pocoo.org/'),
-#          ('You can modify those links in your config file', '#'),
-#)
+LINKS =  (('software source', 'https://github.com/mantaraya36/asuar'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
